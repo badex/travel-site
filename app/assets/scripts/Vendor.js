ac30a177b8067ec1c49874c97b04458504857299
Alex Barrett <al.baroid@gmail.com>
2018-03-09 14:35:14 +0000

    Completed Gulp build task

--- app/assets/scripts/Vendor.js
+++ app/assets/scripts/Vendor.js
@@ -1,3 +1,5 @@
+import '../../temp/scripts/modernizr'
+import 'picturefill';
 import 'lazysizes';
 // make the browser window short and open the network tab in developer tools to
 // see the lazy loading images appear one by one as you scroll
--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
 webpack = require('webpack');
 
-gulp.task('scripts', function(callback) {
+gulp.task('scripts', ['modernizr'], function(callback) {
   console.log('scripts.js was called');
   webpack(require('../../webpack.config.js'), function(err, stats) {
     if(err) {
